939d08c0fd1802ff6e416f623af9b86ed7a5415d
SaiTeja <homehum333@outlook.com>
2020-10-24 05:41:55 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="box">
+      <h1>Chuck Norris Jokes Generator</h1>
+      <p>Here goes the joke</p>
+      <button>Get new joke</button>
+    </div>
+  );
+}
+
+export default App;
