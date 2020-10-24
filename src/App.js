4ac3ede15754006af90b5b3f0aeecfefd63a9e68
SaiTeja <homehum333@outlook.com>
2020-10-24 06:23:15 +0530

    jokes source code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+const API_URL=`https://api.icndb.com/jokes/random`
+const[joke, setJoke]= useState('')
+
+const generateJoke=()=>{
+  fetch(API_URL)
+  .then(res=>res.json())
+  .then(data=>setJoke(data.value.joke))
+}
+
+useEffect(()=>{
+  generateJoke();
+},[])
+
+
   return (
     <div className="box">
-      <h1>Chuck Norris Jokes Generator</h1>
-      <p>Here goes the joke</p>
-      <button>Get new joke</button>
+      <h2>Chuck Norris Jokes Generator</h2>
+  <p dangerouslySetInnerHTML={{__html: joke}}/>
+      <button onClick={generateJoke}>Get new joke 🤣 </button>
     </div>
   );
 }
